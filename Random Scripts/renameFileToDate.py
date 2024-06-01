9b7626caa5580536f538b9fc77c20fc89c86b49b
Tanner <137472577+oxyppgyn@users.noreply.github.com>
2024-06-01 09:16:10 -0400

    Update and rename RenameFileToFileDate.py to renameFileToDate.py

--- Random Scripts/renameFileToDate.py
+++ Random Scripts/renameFileToDate.py
@@ -6,8 +6,10 @@ Python ver. 3.9.16
 import os
 from datetime import datetime
 
-print("WARNING: This script will rename all files AND folders in a given directory. If you only want to rename files, remove any folders that may be renamed.")
-print("")
+dateFormat = '%Y-%m-%d %H-%M-%S'
+
+
+print("WARNING: This script will rename all files AND folders in a given directory. If you only want to rename files, remove any folders that may be renamed.\n")
 
 def rename_file_with_counter(old_filepath, new_filepath):
     counter = 1
@@ -21,7 +23,7 @@ dir = input("Folder containing files to be renamed: ")
 for file in os.listdir(dir):
     old_filepath = os.path.join(dir, file)
     timestamp = os.path.getmtime(old_filepath)
-    date_str = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H-%M-%S')
+    date_str = datetime.fromtimestamp(timestamp).strftime(dateFormat)
     new_name = f'{date_str}{os.path.splitext(file)[1]}'
     new_filepath = os.path.join(dir, new_name)
     rename_file_with_counter(old_filepath, new_filepath)
