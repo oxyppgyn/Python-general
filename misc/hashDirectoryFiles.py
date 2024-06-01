30e36374a85129f18ada9e683402ed7defab84f0
Tanner <137472577+oxyppgyn@users.noreply.github.com>
2024-06-01 09:20:26 -0400

    Add files via upload

---
+++ misc/hashDirectoryFiles.py
@@ -0,0 +1,37 @@
+"""
+Created by: Tanner Hammond
+Date: July 2023 
+Python ver. 3.9.16
+"""
+
+folder_path = input('Path to Folder to be hashed: ' # Input path to directory
+output_file = r'Path to output file (.txt): ' # Input path to output file
+
+import hashlib
+import os
+
+#Generate hashes of files
+def hash_file(file_path):
+    hasher = hashlib.sha256()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            hasher.update(chunk)
+    return hasher.hexdigest()
+
+#Record hashes & filepaths
+def record_hashes(folder_path, output_file):
+    hashes = {}
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_hash = hash_file(file_path)
+            hashes[file_path] = file_hash
+    
+    #Write hashes and file paths to a text file
+    with open(output_file, 'w') as f:
+        for file_path, file_hash in hashes.items():
+            f.write(f"{file_path}: {file_hash}\n")
+
+record_hashes(folder_path, output_file)
+
+print("Done recording hashes of files in", folder_path)
---
+++ misc/renameFileToDate.py
@@ -0,0 +1,31 @@
+"""
+Created July 2023 by Tanner Hammond
+Python ver. 3.9.16
+"""
+
+import os
+from datetime import datetime
+
+dateFormat = '%Y-%m-%d %H-%M-%S'
+
+
+print("WARNING: This script will rename all files AND folders in a given directory. If you only want to rename files, remove any folders that may be renamed.\n")
+
+def rename_file_with_counter(old_filepath, new_filepath):
+    counter = 1
+    while os.path.exists(new_filepath):
+        name, ext = os.path.splitext(new_filepath)
+        new_filepath = f'{name} ({counter}){ext}'
+        counter += 1
+    os.rename(old_filepath, new_filepath)
+
+dir = input("Folder containing files to be renamed: ")
+for file in os.listdir(dir):
+    old_filepath = os.path.join(dir, file)
+    timestamp = os.path.getmtime(old_filepath)
+    date_str = datetime.fromtimestamp(timestamp).strftime(dateFormat)
+    new_name = f'{date_str}{os.path.splitext(file)[1]}'
+    new_filepath = os.path.join(dir, new_name)
+    rename_file_with_counter(old_filepath, new_filepath)
+
+print("Done renaming files.")
